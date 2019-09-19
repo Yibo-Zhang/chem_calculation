208dcb7ef385a6221bbad910eebde9b50a7e31ac
Yibo Zhang <mucherzhang@hotmail.com>
2019-09-19 14:16:12 -0400

    Update read_nwchem_output.py

--- read_nwchem_output.py
+++ read_nwchem_output.py
@@ -4,6 +4,9 @@ class Output(object):
 	def __init__(self):
 		super(Output,self).__init__()
 
+
+
+	####### get optimized structure
 	def load_nwoutput(self,dir):
 		self.structure = []
 		# pattern = re.compile(r'Output coordinates in angstroms')
@@ -40,8 +43,41 @@ class Output(object):
 		with open(name,'a') as f:
 			f.write(structure)
 
+	####### get optimized structure
+
+	####### get energy
+	def get_energy(self,directory):
+		print(directory.split('/')[-2])
+		pattern = re.compile(r'DFT energy')
+		with open(directory,'r') as nwoutput:
+			line = nwoutput.readline()
+			while line:
+				if re.search(pattern,line):
+					print(line,end='\n')
+				line = nwoutput.readline()
+
+
+
+	def get_all_energy(self,dir):
+		from os import walk
+		pattern = re.compile(r'.out$')
+		outs = []
+		for root, dirs, files in walk(dir):
+			# print(root)
+			# print(dirs)
+			# print(files)
+			for i in files:
+				if re.search(pattern,i):
+					outs.append(root+'/'+i)
+		for dir in outs:
+			self.get_energy(dir)
+
+
 
 if __name__ == '__main__':
-	dir = '/Users/Bo/Desktop/restat.out'
+	# dir = '/Users/Bo/Desktop/restat.out'
+	# nwOutPut = Output()
+	# nwOutPut.get_structure(dir)
+	dir = '/Users/Bo/Desktop/close_gradually'
 	nwOutPut = Output()
-	nwOutPut.get_structure(dir)
+	nwOutPut.get_all_energy(dir)
